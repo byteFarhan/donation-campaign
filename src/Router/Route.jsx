761d2e45b3b71b23c9a167060e688891c83dd01c
farhan-71 <farhanmazumder75@gmail.com>
2023-09-27 10:04:05 +0600

    All design part and functionality has been added for detailes page.

--- src/Router/Route.jsx
+++ src/Router/Route.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Root from "../layout/Root/Root";
 import Home from "../pages/Home/Home";
 import Detailes from "../pages/Detailes/Detailes";
+import Donation from "../pages/Donation/Donation";
 
 const route = createBrowserRouter([
   {
@@ -17,6 +18,10 @@ const route = createBrowserRouter([
         element: <Detailes></Detailes>,
         // loader: fetch("/data.json"),
         loader: () => fetch("/data.json"),
+      },
+      {
+        path: "/donation",
+        element: <Donation></Donation>,
       },
     ],
   },
--- src/pages/Detailes/Detailes.jsx
+++ src/pages/Detailes/Detailes.jsx
@@ -1,33 +1,51 @@
 import { useLoaderData, useParams } from "react-router-dom";
+import swal from "sweetalert";
 
 const Detailes = () => {
   const { id } = useParams();
   const datas = useLoaderData();
   // console.log(datas);
   // console.log(id);
-  const {
-    title,
-    background_color,
-    text_color,
-    price,
-    img,
-    description,
-    category_bg_color,
-    category,
-  } = datas.find((data) => data.id === parseInt(id));
+  const data = datas.find((data) => data.id === parseInt(id));
+  const { title, text_color, price, img, description } = data || {};
+  const handleDonateBtn = () => {
+    const donateItemsList = [];
+    const donateItems = JSON.parse(localStorage.getItem("donateItems"));
+    // console.log(donateItems);
+    if (!donateItems) {
+      donateItemsList.push(data);
+      localStorage.setItem("donateItems", JSON.stringify(donateItemsList));
+      swal(
+        "Thank You.😍",
+        "Your donation has been successfully added..",
+        "success"
+      );
+    } else {
+      const isExits = donateItems.find((items) => items.id === parseInt(id));
+      // console.log(isExits);
+      if (!isExits) {
+        donateItemsList.push(data);
+        localStorage.setItem("donateItems", JSON.stringify(donateItemsList));
+        swal("Thank You.😍", "Your donation is successfully added.", "success");
+      } else {
+        swal("Thank You.😍", "Your donation has already been added.", "error");
+      }
+    }
+  };
 
   return (
     <div className="px-5 my-10 md:px-10 lg:px-40">
-      <div className=" h-1/4">
+      <div className="relative h-1/4">
         <img
           src={img}
           alt=""
           className="object-cover w-full lg:h-[750px] max-h-full mb-6 rounded-lg"
         />
-        <div className="relative bottom-0 bg-gradient-to-r from-[#0b0b0b80] to-[#0b0b0b80] bgb ">
+        <div className="absolute w-full p-5 md:px-10 md:py-10 bottom-0 bg-gradient-to-r from-[#0b0b0b80] to-[#0b0b0b80] bgb ">
           <button
+            onClick={handleDonateBtn}
             style={{ backgroundColor: text_color }}
-            className="text-xl font-medium text-white rounded-md btn"
+            className="font-semibold text-white border-none rounded-md md:text-xl btn"
           >
             Donate ${price}
           </button>
@@ -36,7 +54,9 @@ const Detailes = () => {
 
       <div className="space-y-5">
         <h1 className="text-[40px] font-bold">{title}</h1>
-        <p className="text-justify text-[#0b0b0bb3]">{description}</p>
+        <p className="text-justify font-medium text-[#0b0b0bb3]">
+          {description}
+        </p>
       </div>
     </div>
   );
